86abfed7fb3987313ab6a2697307fd0b52aff260
Bricio Velazquez <FortinoBricio_VelazquezHernandez@comcast.com>
2023-07-05 21:42:08 -0400

    Layout and navigation resources

--- app/src/main/java/com/example/rickmortydemogradle7/ui/MainActivity.kt
+++ app/src/main/java/com/example/rickmortydemogradle7/ui/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.rickmortydemogradle7.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.example.rickmortydemogradle7.R
+import com.example.rickmortydemogradle7.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: SharedViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
+        setContentView(binding.root)
     }
 }
